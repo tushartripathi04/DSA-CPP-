0dc696d76001cd80dc903f01b3fcac5dd66ed4c4
tushar tripathi <tushartripathi701@gmail.com>
2025-10-02 10:43:17 +0530

    add

---
+++ accessarray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 4, 8, 12, 16};
+    
+    // Accessing fourth element
+    cout << arr[3] << " ";
+    
+    // Accessing first element
+    cout << arr[0];
+    
+    return 0;
+}
---
+++ updatearray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 4, 8, 12, 16};
+    
+    // Updating first element
+    arr[0] = 90;
+    cout << arr[0] << endl;
+    
+    return 0;
+}

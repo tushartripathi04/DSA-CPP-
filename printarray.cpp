d00dc0829ff66c0a8e80581bc474f151f61d1e32
tushar tripathi <tushartripathi701@gmail.com>
2025-10-02 10:34:49 +0530

    added

---
+++ printarray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declaring and initializing an array of size 5
+    int arr[5] = {2, 4, 9 ,12, 16};
+
+    // printing array elements
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

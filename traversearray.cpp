a5b6f170d0fd65f4c379c87ae6062463e0c5d5bd
tushar tripathi <tushartripathi701@gmail.com>
2025-10-02 10:49:53 +0530

    ad

---
+++ traversearray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {3, 4, 0, 12, 16};
+    
+    // Traversing and printing arr
+    for (int i = 0; i < 5; i++)
+        cout << arr[i] << " ";
+    
+    return 0;
+}
